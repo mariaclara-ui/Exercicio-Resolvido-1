0dd075a66cccfb59db2e67e07f7afe85c2b8d0ae
mariaclara-ui <Mariaclaraduartedeniz@gmail.com>
2026-06-04 22:45:31 -0300

    Delete Exercicio_01_Resolvido directory

--- Exercicio_01_Resolvido/aplicativo/Main.java
+++
@@ -1,7 +0,0 @@
-package Exercicio_01_Resolvido.aplicativo;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
--- Exercicio_01_Resolvido/entidades/enums/status.java
+++
@@ -1,5 +0,0 @@
-package Exercicio_01_Resolvido.entidades.enums;
-
-public enum status {
-//aqui vai ficar os status da pessoa PERGUNTAR NO MAIN//
-}
